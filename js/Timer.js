193c1bb1113a7a64aeff5515d6120ca80be7e93c
AlexZam <alexzzam@gmail.com>
2011-09-27 19:16:38 +0400

    Add close button.
    Also some minor changes.

--- js/Timer.js
+++ js/Timer.js
@@ -12,164 +12,205 @@ Ext.define('alexzam.comp.Timer', {
 
     config:{
         autoRun:true,
-        name: ''
+        name: '',
+        closeButton:true
     },
 
     constructor: function (config) {
-        this.callParent(arguments);
-        this.initConfig(config);
+        var me = this;
+        me.callParent(arguments);
+        me.initConfig(config);
 
-        this.on('afterrender', function(){
-            if(this.drawn) return false;
-            this.drawn = true;
+        me.on('afterrender', function() {
+            if (me.drawn) return false;
+            me.drawn = true;
 
-            this.initGraphic();
-            alexzam.comp.TimerManager.registerTimer(this);
+            me.initGraphic();
+            alexzam.comp.TimerManager.registerTimer(me);
+            me.surface.on('mouseover', me.onMouseOver, me);
+            me.surface.on('mouseout', me.onMouseOut, me);
+            me.surface.on('click', me.onClick, me);
         });
-        this.notified = false;
-        if(Ext.isString(this.config.timeFunc)) this.fnCalc = this.getTimeFuncByName(this.config.timeFunc, this.config.timeFuncParam);
-        else this.fnCalc = this.config.timeFunc;
-        this.running = this.config.autoRun;
-    
-        return this;
+        me.notified = false;
+        if (Ext.isString(me.config.timeFunc))
+            me.fnCalc = me.getTimeFuncByName(me.config.timeFunc, me.config.timeFuncParam);
+        else me.fnCalc = me.config.timeFunc;
+        me.running = me.config.autoRun;
+
+        return me;
     },
 
-    initGraphic:function(){
-            var size = Math.min(this.getWidth(), this.getHeight());
-            var rad = Math.floor(size/2);
-            var space = rad * 0.015;
-	    var wid = rad * 0.05;
-            var trans = {x:rad,y:rad};
+    initGraphic:function() {
+        var size = Math.min(this.getWidth(), this.getHeight());
+        var rad = Math.floor(size / 2);
+        var space = rad * 0.015;
+        var wid = rad * 0.05;
+        var trans = {x:rad,y:rad};
 
-  	    rad *= 0.9;
+        rad *= 0.9;
 
-            /*this.surface.add({
-                type: 'circle',
-                stroke: '#FF3333',
-                radius: rad,
-                x: 0,
-                y: 0,
-                group:'all'
-            });*/
+        /*this.surface.add({
+         type: 'circle',
+         stroke: '#FF3333',
+         radius: rad,
+         x: 0,
+         y: 0,
+         group:'all'
+         });*/
 
-   	    rad -= space;
-	    this.drawSegments(60, 0.85, rad - wid, rad, 's');
-   	    rad -= wid + space;
-	    this.drawSegments(60, 0.85, rad - wid, rad, 'm');
-	    rad -= wid + space;
-	    this.drawSegments(24, 0.9, rad - wid, rad, 'h');
-	    rad -= wid + space;
-	    this.drawSegments(31, 0.85, rad - wid, rad, 'd');
-	    rad -= wid + space;
-	    this.drawSegments(12, 0.93, rad - wid, rad, 'mo');
+        rad -= space;
+        this.drawSegments(60, 0.85, rad - wid, rad, 's');
+        rad -= wid + space;
+        this.drawSegments(60, 0.85, rad - wid, rad, 'm');
+        rad -= wid + space;
+        this.drawSegments(24, 0.9, rad - wid, rad, 'h');
+        rad -= wid + space;
+        this.drawSegments(31, 0.85, rad - wid, rad, 'd');
+        rad -= wid + space;
+        this.drawSegments(12, 0.93, rad - wid, rad, 'mo');
 
-            wid = rad * 0.3;
-	    var text = this.surface.add({
-                type: 'text',
-                x: 0,
-                y: -Math.floor(wid/2),
-                text: "000 000000",
-                font: ''+wid+" Digital",
-                group: ['all', 'time']
-            });
-            this.surface.render(text);
-            text.setAttributes({'text-anchor': 'middle'}, true);
-            text.addCls('text');
+        wid = rad * 0.3;
+        var surface = this.surface;
+        var text = surface.add({
+            type: 'text',
+            x: 0,
+            y: -Math.floor(wid / 2),
+            text: "000 000000",
+            font: '' + wid + " Digital",
+            group: ['all', 'time']
+        });
+        surface.render(text);
+        text.setAttributes({'text-anchor': 'middle'}, true);
+        text.addCls('text');
 
-            wid = rad * 0.2;
-            text = this.surface.add({
-                type: 'text',
-                x: 0,
-                y: Math.floor(wid/2),
-                text: this.config.name.substr(0,10),
-                font: ''+wid+" Digital",
-                group: ['all', 'tname']
-            });
-            this.surface.render(text);
-            text.setAttributes({'text-anchor': 'middle'}, true);
-            text.addCls('text');
+        wid = rad * 0.2;
+        text = surface.add({
+            type: 'text',
+            x: 0,
+            y: Math.floor(wid / 2),
+            text: this.config.name.substr(0, 10),
+            font: '' + wid + " Digital",
+            group: ['all', 'tname']
+        });
+        surface.render(text);
+        text.setAttributes({'text-anchor': 'middle'}, true);
+        text.addCls('text');
 
- 	    this.surface.getGroup('all').setAttributes({
-                translate: trans
-            }, false);
-            this.surface.renderAll(); 
-    },
+        surface.getGroup('all').setAttributes({
+            translate: trans
+        }, false);
 
-    drawSegments: function(n,k,r1,r2,id){
-	var i = 0;
-	while(i<n){
-		var a=2*Math.PI*i/n;
-		var b=2*Math.PI*(i+k)/n;
-                
-		var path = [
-                    ['M',r2*Math.sin(a), -r2*Math.cos(a)],
-                    ['A',r2, r2, 0, 0, 1, (r2*Math.sin(b)), (-r2*Math.cos(b))],
-                    ['L',(r1*Math.sin(b)), (-r1*Math.cos(b))],
-                    ['A',r1, r1, 0, 0, 0, (r1*Math.sin(a)), (-r1*Math.cos(a))],
+        if (this.config.closeButton) {
+            wid = size * .06;
+            var lwid = wid * .25;
+            var l1 = wid / 2 - lwid;
+            var but = surface.add({
+                type: 'path',
+                path: [
+                    ['M', size - 5 - wid, 5 + wid],
+                    ['l', lwid, 0],
+                    ['l', l1, l1],
+                    ['l', l1, -l1],
+                    ['l', lwid, 0],
+                    ['l', 0, lwid],
+                    ['l', -l1, l1],
+                    ['l', l1, l1],
+                    ['l', 0, lwid],
+                    ['l', -lwid, 0],
+                    ['l', -l1, -l1],
+                    ['l', -l1, l1],
+                    ['l', -lwid, 0],
+                    ['l', 0, -lwid],
+                    ['l', l1, -l1],
+                    ['l', -l1, -l1],
                     ['z']
-                ];
-		var sector = this.surface.add({
-                    type: 'path',
-                    path: path,
-                    group:['all',id]
-                });
-                this.surface.render(sector);
-                sector.addCls(['sector', id]);
-		i++;
-	}
+                ],
+                group: 'btClose'
+            });
+            surface.render(but);
+            but.addCls('bt-close');
+        }
+
+        surface.renderAll();
     },
 
-    fillSectors: function(id, num){
-        this.surface.getGroup(id).each(function(spr, i){
-            if(i >= num)spr.addCls('inact');
+    drawSegments: function(n, k, r1, r2, id) {
+        var i = 0;
+        while (i < n) {
+            var a = 2 * Math.PI * i / n;
+            var b = 2 * Math.PI * (i + k) / n;
+
+            var path = [
+                ['M',r2 * Math.sin(a), -r2 * Math.cos(a)],
+                ['A',r2, r2, 0, 0, 1, (r2 * Math.sin(b)), (-r2 * Math.cos(b))],
+                ['L',(r1 * Math.sin(b)), (-r1 * Math.cos(b))],
+                ['A',r1, r1, 0, 0, 0, (r1 * Math.sin(a)), (-r1 * Math.cos(a))],
+                ['z']
+            ];
+            var sector = this.surface.add({
+                type: 'path',
+                path: path,
+                group:['all',id]
+            });
+            this.surface.render(sector);
+            sector.addCls(['sector', id]);
+            i++;
+        }
+    },
+
+    fillSectors: function(id, num) {
+        this.surface.getGroup(id).each(function(spr, i) {
+            if (i >= num)spr.addCls('inact');
             else spr.removeCls('inact');
         });
     },
 
-    setRest: function(msec){
-	var rest = Math.floor(msec / 1000);
-	var cur = rest % 60;
-	var text = ((cur<10)?'0':'') + cur;
-	this.fillSectors('s', cur);
+    setRest: function(msec) {
+        var rest = Math.floor(msec / 1000);
+        var cur = rest % 60;
+        var text = ((cur < 10) ? '0' : '') + cur;
+        this.fillSectors('s', cur);
 
-	rest = Math.floor(rest / 60);
-	cur = rest % 60;
-	text = ((cur<10)?'0':'') + cur + text;
-	this.fillSectors('m', cur);
+        rest = Math.floor(rest / 60);
+        cur = rest % 60;
+        text = ((cur < 10) ? '0' : '') + cur + text;
+        this.fillSectors('m', cur);
 
-	rest = Math.floor(rest / 60);
-	cur = rest % 24;
-	text = ((cur<10)?' 0':' ') + cur + text;
-	this.fillSectors('h', cur);
+        rest = Math.floor(rest / 60);
+        cur = rest % 24;
+        text = ((cur < 10) ? ' 0' : ' ') + cur + text;
+        this.fillSectors('h', cur);
 
-	rest = Math.floor(rest / 24);
-	cur = rest % 31;
-	text = ((cur<10)?'0':'') + cur + text;
-	this.fillSectors('d', cur);
+        rest = Math.floor(rest / 24);
+        cur = rest % 31;
+        text = ((cur < 10) ? '0' : '') + cur + text;
+        this.fillSectors('d', cur);
 
-	rest = Math.floor(rest / 31);
-	text = '' + (rest % 31) + text;
-	this.fillSectors('mo', rest % 12);
+        rest = Math.floor(rest / 31);
+        text = '' + (rest % 31) + text;
+        this.fillSectors('mo', rest % 12);
 
-	this.surface.getGroup('time').getAt(0).setAttributes({text:text}, true);
+        this.surface.getGroup('time').getAt(0).setAttributes({text:text}, true);
     },
 
-    updateTimer: function(){
-        if(!this.running) return;
-	var rest = this.fnCalc();
-	if(rest > 0){
-	    this.setRest(rest);
-	} else if (!this.notified) {
+    updateTimer: function() {
+        if (!this.running) return;
+        var rest = this.fnCalc();
+        if (rest > 0) {
+            this.setRest(rest);
+        } else if (!this.notified) {
             this.running = false;
-	    if (window.webkitNotifications) {
-	        window.webkitNotifications.createNotification(null, 'Timer has finished!', 'Timer '+this.config.name+' came to its zero point.').show();
-	    }
-	    this.notified = true;
-	}
+            if (window.webkitNotifications) {
+                window.webkitNotifications.createNotification(null, 'Timer has finished!',
+                    'Timer ' + this.config.name + ' came to its zero point.')
+                    .show();
+            }
+            this.notified = true;
+        }
     },
 
-    getTimeFuncByName: function(name, param){
-        switch(name){
+    getTimeFuncByName: function(name, param) {
+        switch (name) {
             case "totime":
                 return this.getFuncToTime(param);
             case "timer":
@@ -178,16 +219,37 @@ Ext.define('alexzam.comp.Timer', {
         return null;
     },
 
-    getFuncToTime:function(dt){
-        return function(){
+    getFuncToTime:function(dt) {
+        return function() {
             return dt.getTime() - (new Date()).getTime();
         }
     },
-    getFuncTotalTime:function(lng){
+    getFuncTotalTime:function(lng) {
         var target = (new Date()).getTime() + lng;
-        return function(){
+        return function() {
             return target - (new Date()).getTime();
         }
+    },
+    onMouseOver:function() {
+        this.surface.getGroup('btClose').addCls('bt-close-active');
+    },
+    onMouseOut:function() {
+        this.surface.getGroup('btClose').removeCls('bt-close-active');
+    },
+    onClick:function(ev) {
+        console.dir();
+        var x = ev.browserEvent.offsetX;
+        var y = ev.browserEvent.offsetY;
+        var box = this.surface.getGroup('btClose').getBBox();
+        var bx = box.x;
+        var by = box.y;
+
+        var clicked = (x >= bx && y >= by && x <= bx + box.width && y <= by + box.height);
+        if(clicked) this.remove();
+    },
+    remove:function(){
+        alexzam.comp.TimerManager.removeTimer(this);
+        this.destroy();
     }
 });
 
@@ -197,24 +259,24 @@ Ext.define('alexzam.comp.TimerManager', {
     timers: [],
     running: false,
 
-    registerTimer: function(timer){
-        if(!Ext.Array.contains(alexzam.comp.TimerManager.timers, timer)) alexzam.comp.TimerManager.timers.push(timer);
-        if(!alexzam.comp.TimerManager.running && timer.running) {
+    registerTimer: function(timer) {
+        if (!Ext.Array.contains(alexzam.comp.TimerManager.timers, timer)) alexzam.comp.TimerManager.timers.push(timer);
+        if (!alexzam.comp.TimerManager.running && timer.running) {
             alexzam.comp.TimerManager.running = true;
             alexzam.comp.TimerManager.updateTimers();
         }
     },
 
-    removeTimer: function(timer){
+    removeTimer: function(timer) {
         Ext.Array.remove(alexzam.comp.TimerManager.timers, timer);
     },
 
-    updateTimers:function(){
-        if(!alexzam.comp.TimerManager.running) return;
-	for(i in alexzam.comp.TimerManager.timers){
-	    alexzam.comp.TimerManager.timers[i].updateTimer();
-	}
-	setTimeout(alexzam.comp.TimerManager.updateTimers, 1000);
+    updateTimers:function() {
+        if (!alexzam.comp.TimerManager.running) return;
+        for (i in alexzam.comp.TimerManager.timers) {
+            alexzam.comp.TimerManager.timers[i].updateTimer();
+        }
+        setTimeout(alexzam.comp.TimerManager.updateTimers, 1000);
     }
 
 });
